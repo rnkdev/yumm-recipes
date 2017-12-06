fa5bbbf20724b68c4d7b127320166f5abd37a0bc
rntesla <rnkdev@users.noreply.github.com>
2017-12-06 11:00:18 +1100

    incorporate redux and react router to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './components/App';
+import configureStore from './configureStore';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+const AppRoot = () => (
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(<AppRoot />, document.getElementById('root'));
 registerServiceWorker();
