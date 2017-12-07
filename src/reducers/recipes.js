f31251d62d4374ff6fc505e8aa92fc236aee94ca
rntesla <rnkdev@users.noreply.github.com>
2017-12-07 14:51:12 +1100

    add recipe reducer

--- src/reducers/recipes.js
+++ src/reducers/recipes.js
@@ -1,10 +1,32 @@
+import * as RecipeActions from '../actions/search';
+
 const initialState = {
   main: [],
   saved: [],
+  isLoading: false, 
+  errorMessage: '',
 }
 
 export default function recipes(state = initialState, action) {
   switch (action.type) {
+    case RecipeActions.SET_ERROR_MESSAGE:
+      return {
+        ...state,
+        errorMessage: action.message
+      };
+
+    case RecipeActions.SET_LOADING_STATUS:
+      return {
+        ...state,
+        isLoading: action.status
+      };
+
+    case RecipeActions.RECEIVE_RECIPES:
+      return {
+        ...state,
+        main: action.recipes
+      };
+
     default:
       return state;
   }
