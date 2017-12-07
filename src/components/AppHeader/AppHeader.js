f0d5a81c7d9e44a3df53a0419d58e88928100880
rntesla <rnkdev@users.noreply.github.com>
2017-12-07 11:17:17 +1100

    incorporate search bar into app header

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, NavLink } from 'react-router-dom';
-
+import { Route, Link, NavLink } from 'react-router-dom';
 import { withStyles } from 'material-ui/styles';
+
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
@@ -10,16 +10,27 @@ import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from 'material-ui-icons/Menu';
 
+import DynamicSearchBar from '../../containers/DynamicSearchBar';
+
 const styles = theme => ({
   root: {
     width: '100%',
   },
-  flex: {
+  flexContainer: {
+    display: 'flex',
+  },
+  fullFlex: {
     flex: 1,
+  },
+  halfFlex: {
+    flex: 0.5,
   },
   menuButton: {
     marginLeft: -12,
     marginRight: 20,
+  },
+  savedButton: {
+    textDecoration: 'underline',
   },
 });
 
@@ -32,10 +43,11 @@ function AppHeader(props) {
           <IconButton className={classes.menuButton} color="contrast" aria-label="Menu">
             <MenuIcon />
           </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex} component={Link} to={'/'} style={{textDecoration: 'none'}}>
+          <Typography type="title" color="inherit" className={classes.fullFlex} component={Link} to={'/'} style={{textDecoration: 'none'}}>
             Yummy Recipes
           </Typography>
-          <Button color="contrast" component={Link} to={'/saved'}>Saved</Button>
+          <DynamicSearchBar />
+          <Button color="contrast" component={NavLink} to={'/saved'} activeClassName={classes.savedButton}>Saved</Button>
         </Toolbar>
       </AppBar>
     </div>
