007fe7cb091e489c8b39d0e3c0cbd1fcb0a5124d
rntesla <rnkdev@users.noreply.github.com>
2017-12-08 12:00:08 +1100

    configure store to accept redux dev tools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import mainReducer from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
   return createStore(
     mainReducer,
     {}, // initial state
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeEnhancers(
+      applyMiddleware(thunk)
+    )
   );
 }
