cf48f1a5367840dcf93fbbdcb13bd9e266d67783
rntesla <rnkdev@users.noreply.github.com>
2017-12-08 12:01:38 +1100

    add recipe view page container

--- src/containers/RecipeViewPage.js
+++ src/containers/RecipeViewPage.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { searchRecipe } from '../actions/search';
 
 import RecipeView from '../components/RecipeView';
 
