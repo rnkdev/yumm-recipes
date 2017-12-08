90efd5f2149bb2dc526fe7f697fffc43f89d20e6
rntesla <rnkdev@users.noreply.github.com>
2017-12-08 12:01:06 +1100

    work on displaying all of the recipes in recipe view

--- src/components/RecipeView/RecipeView.js
+++ src/components/RecipeView/RecipeView.js
@@ -1,13 +1,101 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import { GridList, GridListTile, GridListTileBar } from 'material-ui/GridList';
+import Subheader from 'material-ui/List/ListSubheader';
+import IconButton from 'material-ui/IconButton';
+import StarBorderIcon from 'material-ui-icons/StarBorder';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    background: theme.palette.background.paper,
+    marginTop: '14px',
+  },
+  gridList: {
+    width: '100%',
+    minHeight: 800,
+    transform: 'translateZ(0)',
+  },
+  infoText: {
+    marginTop: '32px',
+  }
+});
+
+// Get larger image from google user content
+function getLImg(imageUrl) {
+  let newImageArr = imageUrl.split("=");
+  newImageArr = newImageArr[0];
+
+  return newImageArr;
+}
 
 class RecipeView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      recipes: [],
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.recipes !== this.props.recipes) {
+      const nextQuery = nextProps.recipes.critera && nextProps.recipes.criteria.q ? nextProps.recipes.criteria.q : ''; 
+      const currQuery = this.props.recipes.criteria && this.props.recipes.criteria.q ? this.props.recipes.criteria.q : ''; 
+      // If query is different we have to reset the state recipe array 
+      if (nextQuery !== currQuery) {
+        let newState = this.state;
+        newState.recipes = nextProps.recipes.matches ? nextProps.recipes.matches : [];
+        this.setState(newState);
+      // If query is the same, it's pagination so we add to current recipe
+      } else {
+        let newState = this.state;
+        newState.recipes = newState.recipes.concat(nextProps.recipes.matches ? nextProps.recipes.matches : []);
+        this.setState(newState);
+      }
+    }
+  }
+
   render() {
+    const { classes } = this.props;
     return (
       <div className="RecipeView">
-        This is the list of recipes.
+        <div className={classes.container}>
+          <GridList cellHeight={180} className={classes.gridList}>
+            <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+              <Subheader component="div">
+                Use the search bar above to search for recipe, press enter to begin search...
+              </Subheader>
+            </GridListTile> 
+            {
+              this.state.recipes ? this.state.recipes.map(recipe => (
+                <GridListTile key={recipe.id} cols={1}>
+                  <img src={getLImg(recipe.imageUrlsBySize['90'])} alt={recipe.recipeName}/>
+                  <GridListTileBar
+                    title={recipe.recipeName}
+                    subtitle={<span>Ratings: {recipe.rating}</span>}
+                    actionIcon={
+                      <IconButton>
+                        <StarBorderIcon color="rgba(255, 255, 255, 0.54)" />
+                      </IconButton>
+                    }
+                  />
+                </GridListTile>
+              )) : <div></div>
+            }
+          </GridList>
+        </div>
       </div>
     );
   }
 }
 
-export default RecipeView;
+RecipeView.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(RecipeView);
