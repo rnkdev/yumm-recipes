4441f7658fcebefa632c33b64c85147eb3c65476
rntesla <rnkdev@users.noreply.github.com>
2017-12-18 01:03:56 +1100

    change api domain to https for deployment

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
 export const APP_ID = '895e5882';
 export const APP_KEY = '51c10ca130d36cdac28fe41fd00cbfdc';
 
-export const YUMMLY_DOMAIN = 'http://api.yummly.com/v1/api';
+export const YUMMLY_DOMAIN = 'https://api.yummly.com/v1/api';
